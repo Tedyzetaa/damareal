c86084ede528b2e246e43f065c6e97d2a8682275
Teddy <tedyzetaa@gmail.com>
2026-05-22 20:27:25 -0400

    v1.2.2

--- main.py
+++ main.py
@@ -129,6 +129,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5501",
         "http://localhost:5501",
+        "https://damareal1.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
